48fe489a9bc256490d3e95b8c4268a2edf3823ca
Zhi(Daniel) Gong <127671090+DrZhiGong@users.noreply.github.com>
2024-04-03 13:59:44 -0700

    Update Testmain.cpp

--- Testmain.cpp
+++ Testmain.cpp
@@ -11,15 +11,10 @@ using namespace std;
 
 int main() {
 
-    // read in file named "C2H2.txt" 
-    // create AO object
-    // print out the number of basis functions, number of electrons, and number of atoms
-    // print out the basis set
-
-    AO C2H4_ao("C2H4.txt");
+    AO inputfile_ao("H2.txt");
 
     // cout << "Overlap Matrix for H2: " << endl;
-    vector<BasisFunction> basis_set = C2H4_ao.basis_set;
+    vector<BasisFunction> basis_set = inputfile_ao.basis_set;
 
     map<int, BasisFunction> basis_map;
     for (int i = 0; i < basis_set.size(); i++) {
@@ -32,29 +27,19 @@ int main() {
     // S.print();
 
     // create a CNDO instance
-    CNDO CNDO_C2H4;
-    // compute the gamma matrix
-    // cout << "Compute Gamma Matrix for C2H4: " << endl;
-    // int natoms = C2H4_ao.get_natoms();
-    // cout << "natoms: " << natoms << endl;
-    // arma::mat gamma = CNDO_C2H4.computeGammaMatrix(natoms, basis_set);
-    // gamma.print();
+    CNDO CNDO_input;
 
-    // cout << "Compute Core Hamiltonian Matrix for H: " << endl;
-    // vector<string> atom_types = C2H4_ao.get_atom_types();
-    // arma::mat Hcore = CNDO_C2H4.computeCoreHamiltonianMatrix(atom_types, basis_set);
-    // Hcore.print();
 
-    CNDO_C2H4.updateDensityMatrix(C2H4_ao, "totalDensity");
+    CNDO_input.updateDensityMatrix(inputfile_ao, "totalDensity");
     arma::mat S_deriv = overlapMatrix_derivative(basis_set);
 
     // Print the combined matrix
 
-    cout << "OV_RA" << endl;
+    cout << "Suv_RA" << endl;
     S_deriv.print();
 
     // cout << "Gamma_RA" << endl;
-    // arma::field<arma::vec> gamma_deriv = CNDO_C2H4.createGammaDerivativeMat(basis_map);
+    // arma::field<arma::vec> gamma_deriv = CNDO_input.createGammaDerivativeMat(basis_map);
     // printField(gamma_deriv);
 
 
